530ed17fa47ace9a31fd398b66bd8bee02276368
QuincyJH <81644228+QuincyJH@users.noreply.github.com>
2025-05-26 10:13:11 -0400

    add runs to router

--- server/app/models/run.py
+++ server/app/models/run.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, DateTime, Integer, String, func
 from app.models.base import Base
 
 class Run(Base):
@@ -7,6 +7,6 @@ class Run(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(255), nullable=False)
     data = Column(String)
-    accessed_at = Column(String, default='CURRENT_TIMESTAMP')
-    modified_at = Column(String, default='CURRENT_TIMESTAMP')
-    created_at = Column(String, default='CURRENT_TIMESTAMP')
+    accessed_at = Column(DateTime, server_default=func.now())
+    modified_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime, server_default=func.now())
