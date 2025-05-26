f67a97d3813859d7ae5df5863a87c4d1cf0338a2
QuincyJH <81644228+QuincyJH@users.noreply.github.com>
2025-05-25 21:12:54 -0400

    Format Add new run button

--- client/src/app/dashboard/new-run-card/new-run-card.component.ts
+++ client/src/app/dashboard/new-run-card/new-run-card.component.ts
@@ -1,14 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
-
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-new-run-card',
-  imports: [MatCardModule, MatIconModule],
+  imports: [MatCardModule, MatIconModule, MatButtonModule, CommonModule],
   templateUrl: './new-run-card.component.html',
   styleUrl: './new-run-card.component.scss'
 })
-export class NewRunCardComponent {
 
+export class NewRunCardComponent {
+  recentRun = false;
 }
